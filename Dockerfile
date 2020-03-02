f9a3f578deb04d47979c65b3b0774a813ead19f1
rahul <ra654@njit.edu>
2020-03-02 18:27:42 -0500

    Feature : setup calculator class and tests

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3
 
 ADD src/ src
 
-CMD ["python","./src/CalculatorTests.py"]
+CMD ["python","./src/CalculatorTest.py"]
 
 
---
+++ src/CalculatorTest.py
@@ -0,0 +1,16 @@
+import unittest
+from Calculator import Calculator
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_subtraction(self):
+        test_data = CsvReader("Tests/Data/subtraction.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
