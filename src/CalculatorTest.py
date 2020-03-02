1948f9e8f770c6844a4686ec2fa01868c0232faa
rahul <ra654@njit.edu>
2020-03-02 18:55:53 -0500

    Feature : setup calculator with result property

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,16 +1,19 @@
 import unittest
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.calculator = Calculator()
 
-    def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, Calculator)
+     def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+     def test_results_property_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(Calculator.result, 4)
 
-    def test_subtraction(self):
-        test_data = CsvReader("Tests/Data/subtraction.csv").data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)
+
+
+
+
+if __name__ == 'main':
+    unittest.main()
