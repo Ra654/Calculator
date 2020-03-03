a608028e39117be44a3eb92fe5c22c30c6c27583
rahul <ra654@njit.edu>
2020-03-02 22:26:35 -0500

    Feature : calculator setup for ADD,SUB,MUL,DIV,SQUARE,SQRT

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,10 +1,25 @@
-def addition(a, b):
+def add(a, b):
     return a + b
 
 
 def subtract(a, b):
     return a - b
 
+
+def multiply(a, b):
+    return a * b
+
+
+def division(a, b):
+    return float(b) / float(a)
+
+
+def square(a):
+    return float(a)**2
+
+
+def sqrt(a):
+    return round((float(a)**.5), 2)
 
 
 class Calculator:
@@ -15,10 +30,30 @@ class Calculator:
 
     def add(self, a, b):
         c = a + b
-        self.result = addition(a, b)
+        self.result = add(a, b)
         return self.result
 
     def subtract(self, a, b):
         c = a - b
         self.result = subtract(a, b)
         return self.result
+
+    def multiply(self, a, b):
+        self.result = multiply(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def sqrt(self, a):
+        self.result = sqrt(a)
+        return self.result
+
+
+
+
--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -22,6 +22,26 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(calculator.subtract(2,2), 0)
         self.assertEqual(calculator.result, 0)
 
+    def test_multiply_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.multiply(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
+    def test_division_method_calculator(self):
+        calculator = Calculator()
+        self.assertAlmostEqual(calculator.division(2, 2), 1)
+        self.assertAlmostEqual(calculator.result, 1.0)
+
+    def test_square_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.square(2), 4)
+        self.assertEqual(calculator.result, 4.0)
+
+    def test_sqrt_method_calculator(self):
+        calculator = Calculator()
+        self.assertAlmostEqual(calculator.sqrt(4), 2)
+        self.assertAlmostEqual(calculator.result, 2.0)
+
 
 if __name__ == 'main':
     unittest.main()
